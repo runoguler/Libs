a77d3856c7a943998ea74838854ef3572d88f995
runoguler <onurguler93@gmail.com>
2018-07-22 16:41:20 +0900

    Argument parameters are added

--- cifar-10.py
+++ cifar-10.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
@@ -42,15 +43,20 @@ def test(model, test_loader, device):
 
 
 def main():
-    # hyper parameters
-    training_batch_size = 64
-    testing_batch_size = 64
-    learning_rate = 0.01
-    momentum = 0.9
-    epochs = 10
+    parser = argparse.ArgumentParser(description="Parameters for Training CIFAR-10")
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR', help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M', help='SGD momentum (default: 0.5)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+    parser.add_argument('--num-workers', type=int, default=1, metavar='N', help='number of workers for cuda')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    cuda_args = kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    cuda_args = {'num_workers': args.num_workers, 'pin_memory': True} if use_cuda else {}
 
     data_transform = transforms.Compose([
         transforms.ToTensor(),
@@ -58,14 +64,14 @@ def main():
     ])
     cifar_training_data = datasets.CIFAR10("../data/CIFAR10", train=True, transform=data_transform, download=True)
     cifar_testing_data = datasets.CIFAR10("../data/CIFAR10", train=False, transform=data_transform)
-    train_loader = torch.utils.data.DataLoader(cifar_training_data, batch_size=training_batch_size, shuffle=True, **cuda_args)
-    test_loader = torch.utils.data.DataLoader(cifar_testing_data, batch_size=testing_batch_size, shuffle=True, **cuda_args)
+    train_loader = torch.utils.data.DataLoader(cifar_training_data, batch_size=args.batch_size, shuffle=True, **cuda_args)
+    test_loader = torch.utils.data.DataLoader(cifar_testing_data, batch_size=args.test_batch_size, shuffle=True, **cuda_args)
 
     model = Net().to(device)
     # model = MobileNetV2().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-    for epoch in range(1, epochs + 1):
+    for epoch in range(1, args.epochs + 1):
         train(epoch, model, train_loader, optimizer, device)
         test(model, test_loader, device)
 
